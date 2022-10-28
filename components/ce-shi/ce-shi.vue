a50ce9d188e6d5b3ec9f55746a279b2634401acd
jan1014 <1179507458@qq.com>
2022-10-28 19:34:21 +0800

    compoents添加规范

---
+++ components/ce-shi/ce-shi.vue
@@ -0,0 +1,20 @@
+<template>
+	<view>
+		
+	</view>
+</template>
+
+<script>
+	export default {
+		name:"ce-shi",
+		data() {
+			return {
+				
+			};
+		}
+	}
+</script>
+
+<style lang="scss">
+
+</style>
